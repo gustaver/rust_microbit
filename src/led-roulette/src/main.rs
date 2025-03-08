f8700aeef872f66605caab7860056ae41fb31cd0
Gustave Rousselet <gustave.rousselet@icloud.com>
2025-03-08 16:11:23 +0100

    LED Roulette

---
+++ src/led-roulette/src/main.rs
@@ -0,0 +1,48 @@
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+use rtt_target::rtt_init_print;
+use panic_rtt_target as _;
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::{prelude::*, Timer},
+};
+
+const PIXELS: [(usize, usize); 16] = [
+    (0,0), (0,1), (0,2), (0,3), (0,4), (1,4), (2,4), (3,4), (4,4),
+    (4,3), (4,2), (4,1), (4,0), (3,0), (2,0), (1,0)
+];
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+    let mut light_matrix = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
+
+    for &(i, j) in PIXELS.iter().cycle() {
+        light_matrix[i][j] = 1;
+
+        // Show light_matrix for 10ms
+        display.show(&mut timer, light_matrix, 10);
+
+        // clear the display
+        display.clear();
+        light_matrix[i][j] = 0;
+
+        // block
+        timer.delay_ms(10_u32);
+    }
+    unreachable!();
+}
